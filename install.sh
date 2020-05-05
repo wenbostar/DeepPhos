ebc4e2887e0675ecce99ff7ebbd66c0bd81adb7b
Bo <wenbostar@gmail.com>
2020-05-05 15:27:30 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
-pip install tensorflow-gpu==1.15
+conda create -n DeepPhos pip python=3.7.6
+conda activate DeepPhos
 pip install keras==2.0.0
 pip install numpy
 pip install pandas
 pip install matplotlib
 pip install scikit-learn
+pip uninstall tensorflow tensorflow-estimator
+pip install tensorflow-gpu==1.15
